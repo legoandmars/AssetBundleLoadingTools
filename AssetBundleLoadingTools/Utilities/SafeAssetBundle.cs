2c8e1983b913e939fde9a538ecc05938c6f8eee2
legoandmars <legoandmars@gmail.com>
2023-05-05 01:24:35 -0700

    Remove unnecessary constructors & add LoadAsset<T>

--- AssetBundleLoadingTools/Utilities/SafeAssetBundle.cs
+++ AssetBundleLoadingTools/Utilities/SafeAssetBundle.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace AssetBundleLoadingTools.Utilities
 {
@@ -16,39 +17,18 @@ namespace AssetBundleLoadingTools.Utilities
         //internal static Dictionary<AssetBundle, string> HashedBundles = new();
         //internal static Dictionary<AssetBundle, byte[]> BundleBytes = new();
 
-        private string _hash; // needed for cache. probably no reasonable way around reading the data twice
         private AssetBundle _bundle;
+        private string _hash; // needed for cache. probably no reasonable way around reading the data twice
         private AssetBundleInitializationType _initializationType;
         private string _bundlePath; // I really hate this. Unfortunately, assettools.net NEEDS a path to extract bundles. Toni please save me
         // If we have to keep the _bundlePath bit, maybe add a way to disable it, it will hurt performance unnecessarily if you don't plan on using shader replacement
 
-        public SafeAssetBundle(AssetBundle bundle, string path)
+        public SafeAssetBundle(AssetBundle bundle, AssetBundleInitializationType initializationType, string hash, string path)
         {
-            _initializationType = AssetBundleInitializationType.File;
-            _hash = AssetBundleHashing.FromFile(path);
             _bundle = bundle;
+            _initializationType = initializationType;
+            _hash = hash;
             _bundlePath = path;
-        }
-
-        public SafeAssetBundle(AssetBundle bundle, byte[] binary)
-        {
-            _initializationType = AssetBundleInitializationType.Memory;
-            _hash = AssetBundleHashing.FromBytes(binary);
-            _bundle = bundle;
-
-            _bundlePath = Path.GetTempFileName();
-            File.WriteAllBytes(_bundlePath, binary);
-        }
-
-        public SafeAssetBundle(AssetBundle bundle, MemoryStream stream)
-        {
-            _initializationType = AssetBundleInitializationType.Stream;
-            _hash = AssetBundleHashing.FromStream(stream);
-            _bundle = bundle;
-
-            _bundlePath = Path.GetTempFileName();
-            File.WriteAllBytes(_bundlePath, stream.ToArray());
-            stream.Dispose();
         }
 
         // i have **NEVER** seen anybody actually use these crc/offset methods
@@ -60,14 +40,18 @@ namespace AssetBundleLoadingTools.Utilities
             // TODO: test exceptions are the same
             // loadfromfile stuff here
             var bundle = AssetBundle.LoadFromFile(path, crc, offset);
-            return new SafeAssetBundle(AssetBundle.LoadFromFile(path, crc, offset), path);
+            return new SafeAssetBundle(bundle, AssetBundleInitializationType.File, AssetBundleHashing.FromFile(path), path);
         }
 
         public static SafeAssetBundle LoadFromMemory(byte[] binary) => LoadFromMemory(binary, 0u);
         public static SafeAssetBundle LoadFromMemory(byte[] binary, uint crc)
         {
             var bundle = AssetBundle.LoadFromMemory(binary, crc);
-            return new SafeAssetBundle(bundle, binary);
+
+            var bundlePath = Path.GetTempFileName();
+            File.WriteAllBytes(bundlePath, binary);
+
+            return new SafeAssetBundle(bundle, AssetBundleInitializationType.Memory, AssetBundleHashing.FromBytes(binary), bundlePath);
         }
 
         public static SafeAssetBundle LoadFromStream(Stream stream) => LoadFromStream(stream, 0u, 0u);
@@ -80,8 +64,18 @@ namespace AssetBundleLoadingTools.Utilities
             var memoryStream = new MemoryStream();
             stream.CopyTo(memoryStream);
 
+            var hash = AssetBundleHashing.FromStream(memoryStream);
+            var bundlePath = Path.GetTempFileName();
+            File.WriteAllBytes(bundlePath, memoryStream.ToArray());
+            memoryStream.Dispose();
+
             // **pretty* sure we don't need to dispose stream but it's worth double checking
-            return new SafeAssetBundle(bundle, memoryStream);
+            return new SafeAssetBundle(bundle, AssetBundleInitializationType.Stream, hash, bundlePath);
+        }
+
+        public T LoadAsset<T>(string name) where T : Object
+        {
+            return AssetBundleExtensions.LoadAssetSafe<T>(_bundle, name, _hash);
         }
     }
 }
